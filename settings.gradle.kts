12355f66535cb30017943df825edbeff91376bce
ahmedayman123hack-hash <ahmedayman123.hack@gmail.com>
2026-06-05 03:42:56 +0300

    Add plugin and dependency resolution management

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolution {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+    }
+}
 
+rootProject.name = "PhantomRAT"
+include(":app")
